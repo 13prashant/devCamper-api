951e4ed378e4a4184c5b32a44c23b7907723ac3f
Prashant Patel <pacific1392@gmail.com>
2021-11-23 22:18:52 +0530

    added routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+// Load route files
+const bootcamps = require('./routes/bootcamps')
+
 // Load env vars
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+
+// Routers
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
