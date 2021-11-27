e89defe9b1ac23b761fa5317e198662dbdbc4545
Prashant Patel <pacific1392@gmail.com>
2021-11-27 22:51:06 +0530

    connected mongoDB

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
+const connectDB = require('./config/db')
+
+// Load env vars
+dotenv.config({ path: './config/config.env' })
 
 // Load route files
 const bootcamps = require('./routes/bootcamps')
 
-// Load env vars
-dotenv.config({ path: './config/config.env' })
+// Connect to databae
+connectDB()
 
 const app = express()
 
@@ -18,4 +22,14 @@ app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(
+    PORT,
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    // Close server & exit process
+    server.close(() => process.exit(1))
+})
