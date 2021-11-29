eaafa81d96ee9576e13d7489110e98ada91bbfcf
Prashant Patel <pacific1392@gmail.com>
2021-11-29 22:46:40 +0530

    added custom errorHandler middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const ErrorResponse = require('../utils/errorResponse')
+
 const Bootcamp = require('../models/Bootcamp')
 
 // @desc        Get all bootcapms
@@ -21,12 +23,12 @@ exports.getBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findById(req.params.id)
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false })
+            return next(new ErrorResponse(`Resource not found with the id of ${req.params.id}!`, 404))
         }
 
         res.status(200).json({ success: true, data: bootcamp })
-    } catch (error) {
-        res.status(400).json({ success: false })
+    } catch (err) {
+        next(err)
     }
 }
 
@@ -42,7 +44,7 @@ exports.createBootcamp = async (req, res, next) => {
             msg: bootcamp
         })
     } catch (err) {
-        res.status(400).json({ success: false })
+        next(err)
     }
 
 }
@@ -58,11 +60,11 @@ exports.updateBootcamp = async (req, res, next) => {
         })
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false })
+            return next(new ErrorResponse(`Resource not found with the id of ${req.params.id}!`, 404))
         }
         res.status(200).json({ success: true, data: bootcamp })
-    } catch (error) {
-        res.status(400).json({ success: false })
+    } catch (err) {
+        next(err)
     }
 }
 
@@ -74,10 +76,10 @@ exports.deleteBootcamp = async (req, res, next) => {
         const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
         if (!bootcamp) {
-            return res.status(400).json({ success: false })
+            return next(new ErrorResponse(`Resource not found with the id of ${req.params.id}!`, 404))
         }
         res.status(200).json({ success: true, data: {} })
-    } catch (error) {
-        res.status(400).json({ success: false })
+    } catch (err) {
+        next(err)
     }
 }
